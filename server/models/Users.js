d3e8149881a230d9e81cb3afd92f0d957edd4a6e
nurovic <mertnurovic@gmail.com>
2022-02-17 14:31:23 +0300

    getOwnCourse

--- server/models/Users.js
+++ server/models/Users.js
@@ -1,27 +1,35 @@
-const Mongoose = require("mongoose")
+const Mongoose = require("mongoose");
 
-const UserSchema = new Mongoose.Schema({
+const UserSchema = new Mongoose.Schema(
+  {
     full_name: String,
     password: String,
     email: String,
     profile_image: String,
-    learnings: [{
+    learnings: [
+      {
         type: Mongoose.Schema.Types.ObjectId,
-        ref: 'course',
-        autopopulate: { maxDepth: 1, select: "course_name description" }
-    }],
-    created_projects:
-        [{
+        ref: "course",
+        autopopulate: { maxDepth: 1, select: "course_name description" },
+      },
+    ],
+    created_projects: [
+      {
+        type: Mongoose.Schema.Types.ObjectId,
+        ref: "project",
+        autopopulate: { maxDepth: 1, select: "project_file comment" },
+      },
+    ],
+    created_courses: [
+        {
             type: Mongoose.Schema.Types.ObjectId,
-            ref: 'project',
-            autopopulate: { maxDepth: 1, select: "project_file comment" }
-        }
-        ]
+            ref: "course",
+            autopopulate: { maxDepth: 1, select: "course_name cover" },
+          },
+    ]
+  },
+  { timestamps: true, versionKey: false }
+);
 
-
-},
-    { timestamps: true, versionKey: false }
-)
-
-UserSchema.plugin(require('mongoose-autopopulate'))
-module.exports = Mongoose.model("user", UserSchema) 
+UserSchema.plugin(require("mongoose-autopopulate"));
+module.exports = Mongoose.model("user", UserSchema);
--- server/services/BaseService.js
+++ server/services/BaseService.js
@@ -35,9 +35,6 @@ class BaseService {
     delete(id){
         return this.model.findByIdAndDelete(id)
     }
-    removeOrder(){
-        return console.log(this.model.remove())
-    }
 }
 
 module.exports = BaseService
